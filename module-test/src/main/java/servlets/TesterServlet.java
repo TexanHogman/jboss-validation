b521d00c9d9d1772fe3b82d111a47a11caf0e14a
rhogge <rhogge@paypal.com>
2013-10-07 14:07:21 -0500

    working on it.

--- module-test/src/main/java/servlets/TesterServlet.java
+++ module-test/src/main/java/servlets/TesterServlet.java
@@ -1,24 +1,49 @@
 package servlets;
 
 import java.io.IOException;
+import java.sql.Connection;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.inject.Inject;
+import javax.naming.InitialContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 import org.hogdev.play.Foo;
-
 
 @WebServlet({ "/doit" })
 public class TesterServlet extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
+
+	private @Inject
+	Foo foo;
 	
-	private @Inject Foo foo;
-	
+	//tried quickly to use @Resource but ran into some issue so I'll just  get it in the PostConstruct
+//	@Resource(mappedName="java:jboss/datasources/MyDS")
+	private DataSource dataSource;
+
+	@PostConstruct
+	private void myInit()
+	{
+		System.out.println("Servlet POST CONSTRUCT");
+		
+		try
+		{
+			InitialContext ic = new InitialContext();  // JNDI initial context
+		    dataSource = (DataSource) ic.lookup("java:jboss/datasources/MyDS"); // JNDI lookup			
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
@@ -26,13 +51,24 @@ public class TesterServlet extends HttpServlet
 	{
 		super();
 	}
-	
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
+		Connection conn = null;
+		try
+		{
+			conn = dataSource.getConnection();
+			System.out.println("RQ: " + conn.isReadOnly());
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		
 		response.setCharacterEncoding("UTF-8");
 		response.getWriter().println("If you see this, I guess things worked --- here is the output though: " + foo.foos());
 	}
